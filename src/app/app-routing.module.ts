97b5de93ce3eb5cdfe47da91403577f9e640fdf2
alanlan <alanlant25@gmail.com>
2024-10-29 20:20:09 -0600

    Creacion del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'faqs', component: FaqsComponent },
+  { path: 'sobre-nosotros', component: SobreNosotrosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ServiciosComponent,
+    FaqsComponent,
+    SobreNosotrosComponent
   ],
   imports: [
     BrowserModule,
