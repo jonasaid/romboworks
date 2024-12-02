dcc3dd39acfe62ac4de76dfa22cb60f50cab17da
JeyFlogui <107779982+JeyFlogui@users.noreply.github.com>
2024-12-01 20:26:58 -0600

    servicios.component.ts

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.css']
 })
-export class ServiciosComponent implements OnInit {
+export class ServiciosComponent {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  irAboutUs() {
+    this.router.navigate(['/sobre-nosotros']);
   }
-
 }
